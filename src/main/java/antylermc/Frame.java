619658cf4da43a3562ac7368f7315d93f17b4e76
Antyle_YT <152796256+AntyleYT@users.noreply.github.com>
2024-08-02 19:04:43 +0200

    ○

--- src/main/java/antylermc/Frame.java
+++ src/main/java/antylermc/Frame.java
@@ -1,5 +1,8 @@
 package antylermc;
 
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
 import fr.theshark34.openlauncherlib.util.Saver;
 import fr.theshark34.swinger.util.WindowMover;
 
@@ -47,9 +50,22 @@ public class Frame extends JFrame {
         }
 
         instance = new Frame();
+
+        launchrpc();
     }
 
+    public static void launchrpc() {
+        final DiscordRPC lib = DiscordRPC.INSTANCE;
+        final String appID = "1268975373081645056";
+        final DiscordEventHandlers handlers = new DiscordEventHandlers();
+        lib.Discord_Initialize(appID, handlers, true, "");
+        DiscordRichPresence discordRichPresence = new DiscordRichPresence();
+        discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000;
+        discordRichPresence.details = "Playing AntylerMC";
+        discordRichPresence.state = "AntylerMC";
 
+        lib.Discord_UpdatePresence(discordRichPresence);
+    }
 
     public static Image getImage(String fichier) throws IOException{
         InputStream inputStream = Frame.getInstance().getClass().getClassLoader().getResourceAsStream(fichier);
